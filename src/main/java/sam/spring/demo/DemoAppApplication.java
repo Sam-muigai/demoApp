f3bf3b40e7dc3c822ba94342703311e2128e868c
sammu <muigai.samuel21@students.dkut.ac.ke>
2023-05-16 19:18:18 +0300

    Added variables for railway

--- src/main/java/sam/spring/demo/DemoAppApplication.java
+++ src/main/java/sam/spring/demo/DemoAppApplication.java
@@ -1,13 +1,29 @@
 package sam.spring.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoAppApplication {
+public class DemoAppApplication implements CommandLineRunner {
+
+    private final DemoRepository repository;
+
+    public DemoAppApplication(DemoRepository repository) {
+        this.repository = repository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoAppApplication.class, args);
     }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        Tests example = new Tests(null,"Samuel","I love coding");
+        if (repository.count() == 0){
+            repository.save(example);
+        }
+     }
 
 }
